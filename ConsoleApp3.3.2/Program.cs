6baff947a27d43091ea6ba0f4bfa59fcb48c5082
AndreyChuck <a.chucklanov@yandex.ru>
2023-02-28 21:49:53 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp3.3.2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp3._3._2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string MyName = "Иван";
+            byte MyAge = 30;
+            bool HaveIPet = true;
+            double MyShoeSize = 47;
+
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("My age is " + MyAge);
+            Console.WriteLine("Do i have a pet? " + HaveIPet);
+            Console.WriteLine("My shoes size is " + MyShoeSize);
+        }
+    }
+}
